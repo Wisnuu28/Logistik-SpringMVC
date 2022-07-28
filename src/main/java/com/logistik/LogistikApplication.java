6a9dda5832152ef397892ef406aa19fcb4b9bde5
Wisnu Yudhistira <wisnuyudhistira08@gmail.com>
2022-07-28 21:45:58 +0700

    spring mvc

---
+++ src/main/java/com/logistik/LogistikApplication.java
@@ -0,0 +1,13 @@
+package com.logistik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogistikApplication.class, args);
+	}
+
+}
